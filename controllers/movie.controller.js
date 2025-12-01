394d8c2a4d0656901f8508e24b4da26cb52c52d5
Saroj_Dev <sarojtech0005@gmail.com>
2025-12-01 17:31:00 +0530

    Feature: Added delete & get api for movies

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,4 +1,5 @@
 const Movie = require('../models/movie.model');
+const movieService = require('../services/movie.service');
 
 /**
  * Controller function to create a new movie
@@ -6,6 +7,20 @@ const Movie = require('../models/movie.model');
  * @param {*} res {response object}
  * @returns movie object in JSON format
  */
+
+const errorResponseBody = {
+    err: {},
+    data: {},
+    message: 'Something went wrong, please try again later',
+    success: false
+}
+
+const successResponseBody = {
+    err: {},
+    data: {},
+    message: 'Successfully processed request',
+    success: true
+}
 
 const createMovie = async (req, res) => {
     try {
@@ -25,6 +40,42 @@ const createMovie = async (req, res) => {
     }
 }
 
+const deleteMovie = async (req, res) => {
+    try {
+        const movieId = req.params._id;
+        const movie = await Movie.deleteOne(movieId);
+        return res.status(200).json({
+            success: true,
+            data: movie,
+            message: 'Movie deleted successfully'
+        });
+    } catch (error) {
+        console.error('Error creating movie:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+const getMovie = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const response = await movieService.getMovieById(id);
+        if(response.err) {
+            errorResponseBody.err = response.err;
+            return res.status(response.code).json(errorResponseBody);
+        }
+        successResponseBody.data = response;
+        return res.status(200).json(successResponseBody);
+    } catch (err) {
+        console.error('Error creating movie:', err);
+        return res.status(500).json(errorResponseBody);
+    }
+}
+
 module.exports = {
-    createMovie
+    createMovie,
+    deleteMovie,
+    getMovie
 }
---
+++ services/movie.service.js
@@ -0,0 +1,25 @@
+const Movie = require('../models/movie.model');
+const mongoose = require('mongoose');
+
+const getMovieById = async (id) => {
+
+    if (!mongoose.isValidObjectId(id)) {
+        return {
+            err: "Invalid movie ID format",
+            code: 400
+        };
+    }
+
+    const movie = await Movie.findById(id);
+
+    if (!movie) {
+        return {
+            err: "Cannot find the movie with the given ID",
+            code: 404
+        };
+    }
+
+    return movie;
+};
+
+module.exports = { getMovieById };
